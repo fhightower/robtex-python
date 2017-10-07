f091ad3bdfc93d91b86f33a38404a118ca6f0a28
Floyd Hightower <floyd.hightower27@gmail.com>
2017-10-07 20:33:42 -0300

    Adding tests

--- tests/test_robtex_python.py
+++ tests/test_robtex_python.py
@@ -8,40 +8,21 @@ test_robtex_python
 Tests for `robtex_python` module.
 """
 
-import os
 import docopt
 import pytest
-
-
 
 from robtex_python import robtex_python
 from robtex_python import cli
 
 
 @pytest.fixture
-def absolute_path():
-    """Return an absolute path (which is useful for running tests)."""
-    # return os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(absolute_path, response):
-    """Sample pytest test function with the pytest fixture as an argument.
-    """
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-@pytest.fixture
 def command_line_args():
     """Function to simulate command line arguments using docopt."""
     args = dict()
+
+    args['ip'] = "8.8.8.8"
+    args['asn'] = "15169"
+    args['host'] = "example.com"
 
     # TODO: Add command line arguments here (see: https://github.com/docopt/docopt#api)
 
@@ -58,3 +39,34 @@ def test_command_line_interface(command_line_args):
     error_message = exc_info.value
     # make sure the error message contains the expected usage output
     assert "Usage:" in str(error_message)
+
+
+def test_ip_query(command_line_args):
+    """Test the command line usage of this project."""
+    response = robtex_python.ip_query(command_line_args['ip'])
+
+    assert response['status'] == "ok"
+    assert response['city'] == "Mountain View"
+
+
+def test_as_query(command_line_args):
+    """Test the command line usage of this project."""
+    response = robtex_python.as_query(command_line_args['asn'])
+
+    assert response['status'] == "ok"
+
+
+def test_pdns_forward_query(command_line_args):
+    """Test the command line usage of this project."""
+    response = robtex_python.pdns_forward(command_line_args['host'])
+
+    assert len(response) > 1
+    assert response[0]['rrname'] == command_line_args['host']
+
+
+def test_pdns_reverse_query(command_line_args):
+    """Test the command line usage of this project."""
+    response = robtex_python.pdns_reverse(command_line_args['ip'])
+
+    assert len(response) > 1
+    assert response[0]['rrdata'] == command_line_args['ip']
